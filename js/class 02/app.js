be255a94f80cbbf0c2864e146948681d42771be2
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-09-03 21:06:59 +0500

    Add files via upload

---
+++ js/class 02/app.js
@@ -0,0 +1,88 @@
+// varaibles
+
+var abc = "faraz";
+
+var num = 100;
+
+console.log(abc + " " + num);
+
+// dataTypes
+
+// Number;
+// String;
+// undefined
+
+alert("Muhammad faraz");
+
+var firstname = "Muhammad";
+
+var lastname = "faraz";
+
+console.log(firstname + " " + lastname);
+
+var fullname = "Muhammad faraz";
+
+var city = "karachi";
+
+var education = "bachelor";
+
+console.log(
+  "Hello my name is Muhammad Faraz. I lived in karachi and my education in bachelors."
+);
+
+console.log(
+  "Hello my name is " +
+    fullname +
+    ". I lived in " +
+    city +
+    " and my education in " +
+    education
+);
+
+var num1 = 10;
+
+var num2 = 50;
+
+var res = num1 + num2;
+
+console.log(typeof res);
+
+// variable decalaration
+
+var age;
+
+console.log(age);
+
+// variable decalaration and initialization
+
+var age = 20;
+
+console.log(age);
+
+var age = 20;
+
+console.log(age);
+
+age = 20;
+
+var fullname = "Muhammad faraz",
+  city = "karachi",
+  education = "bachelor";
+
+console.log(
+  "Hello my name is Muhammad Faraz. I lived in karachi and my education in bachelors."
+);
+
+// Variable Names Legal and Illegal
+
+// Camel Case
+
+var firstName = "faraz";
+
+var lastName = "ahmed";
+
+// snake case
+
+var first_name = "faraz";
+
+var last_name = "ahmed";
