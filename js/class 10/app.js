e1310e65a011feb352d271da67b97d45ad3fa15b
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-10-02 18:26:59 +0500

    Add files via upload

---
+++ js/class 10/app.js
@@ -0,0 +1,55 @@
+// palindrome program
+
+// level
+// wow
+// madam
+// civic
+
+// var input = prompt("Enter any word");
+// var reverseWord = "";
+
+// for (var i = input.length - 1; i >= 0; i--) {
+//   reverseWord = reverseWord + input[i];
+// }
+
+// if (input === reverseWord) {
+//   console.log("it's a palindrome word");
+// } else {
+//   console.log("it's not a palindrome word");
+// }
+
+// console.log("input value: ", input);
+
+// console.log("reverseWord value: ", reverseWord);
+
+// var date = new Date();
+
+// console.log(date);
+
+// var daysArr = ["اتوار", "پیر", "منگل", "بدھ", "جمعرات", "جمعہ", "ہفتہ"];
+
+// console.log(daysArr[date.getDay()]);
+
+// console.log(date.toLocaleTimeString());
+
+// var date = new Date();
+
+// console.log(date.setFullYear("1983"));
+
+// var FirstRamadan = new Date("March 1, 2025").getTime();
+
+// var currentDate = new Date().getTime();
+
+// var diff = FirstRamadan - currentDate;
+
+// var month = diff / (1000 * 60 * 60 * 24 * 30);
+
+// console.log(Math.round(month));
+
+// console.log(false > 4);
+
+// console.log(3 > 2 > 1);
+
+// console.log(1 > 2 > 3);
+
+// console.log(1 < 2 < 3);
