450ccd8d4a1e7b23065cca34398cf848357477af
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-10-22 21:20:08 +0500

    Add files via upload

---
+++ js/class 16/app.js
@@ -0,0 +1,15 @@
+console.log(document.childNodes[0]
+
+.childNodes[1].childNodes);
+
+var div = document.getElementById("main");
+
+console.log(div.nodeName);
+
+console.log(div.firstChild);
+
+console.log(div.lastChild);
+
+console.log(div.parentElement);
+
+console.log(div.childNodes[0].nodeType);
