e002fed2f531700a1a475e141a4562866b19d262
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-10-25 21:31:08 +0500

    Add files via upload

---
+++ js/class 17/app.js
@@ -0,0 +1,79 @@
+// // console.log(document.childNodes[1].childNodes[2].childNodes[3]);
+
+// var divELement = document.getElementById("main");
+
+// console.log(divELement.hasAttribute("class"));
+
+// console.log(divELement.getAttribute("class"));
+
+// var anchor = document.getElementById("link");
+
+// anchor.setAttribute("class", "link");
+
+// // console.log();
+
+// var btn = document.getElementById("btn");
+
+// btn.setAttribute("class", "btn");
+
+// btn.setAttribute("onclick", "greet()");
+
+// console.log(btn.attributes[2].value);
+
+// function greet() {
+//   alert("alert calls...");
+// }
+
+// var div =
+
+// document.getElementById("main");
+
+// var h1ELement =
+
+// document.createElement("h1");
+
+// var h1Text =
+
+// document.createTextNode("hello world");
+
+// h1ELement.appendChild(h1Text);
+
+// div.appendChild(h1ELement);
+
+// console.log(h1ELement);
+
+// // console.log(h1Text);
+
+// var div = document.getElementById("main");
+
+// div.className = "container";
+
+// div.setAttribute("class", "container");
+
+// console.log(div);
+
+// var btnElement = document.createElement("button");
+
+// var btnText = document.createTextNode("signup");
+
+// var div = document.getElementById("main");
+
+// btnElement.appendChild(btnText);
+
+// btnElement.setAttribute("onclick", "signUp(this)");
+
+// div.appendChild(btnElement);
+
+// console.log(btnElement);
+
+
+// function signUp(e) {
+//   console.log(e);
+// }
+
+// CRUD Opeartion
+
+// C = CREATE
+// R = READ
+// U = UPDATE
+// D = DELETE
